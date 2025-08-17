f9fb672bd4bdc51080858dd6f5350a7a01eec2cb
Asdasd7117 <alywm100@gmail.com>
2025-08-17 14:18:22 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
-const path = require('path');
-
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 // Serve static files
 app.use(express.static('public'));
@@ -20,5 +18,5 @@ app.use('/p', createProxyMiddleware({
 }));
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Proxy server running on http://localhost:${PORT}`);
 });
