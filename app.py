455624a94487d8ac30451c6b1f361d8d0951c834
Asdasd7117 <alywm100@gmail.com>
2025-09-06 02:24:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,10 @@
+# app.py
 from flask import Flask, render_template
-from flask_socketio import SocketIO
-
 app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins="*")
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-# استقبال حدث المزامنة من التبويبة الرئيسية
-@socketio.on("sync_event")
-def handle_sync(data):
-    socketio.emit("update_tabs", data)
-
 if __name__ == "__main__":
-    # تجاوز مشكلة Werkzeug في Render
-    socketio.run(app, host="0.0.0.0", port=5000, allow_unsafe_werkzeug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True, allow_unsafe_werkzeug=True)
