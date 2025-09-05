b85f272e85d901a5126065bd9523ab169244323c
Asdasd7117 <alywm100@gmail.com>
2025-09-06 02:31:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-# app.py
-from flask import Flask, render_template
+from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+def home():
+    return "Hello World"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True, allow_unsafe_werkzeug=True)
+    # بدّل هذا السطر:
+    # app.run(host="0.0.0.0", port=5000, debug=True, allow_unsafe_werkzeug=True)
+    
+    # إلى هذا:
+    app.run(host="0.0.0.0", port=5000, debug=True)
