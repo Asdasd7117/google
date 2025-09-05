2f874ee59200586aeefe3cb0f55fde624471af2e
Asdasd7117 <alywm100@gmail.com>
2025-09-06 01:36:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,29 @@
-# app.py
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
-sync_enabled = False  # التحكم بالمزامنة
+# لتخزين النصوص المشتركة بين التبويبات
+shared_text = ""
 
-@app.route('/')
+# صفحة رئيسية
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html", text=shared_text)
 
+# استقبال النصوص الجديدة من أي مستخدم
+@socketio.on('update_text')
+def handle_update_text(data):
+    global shared_text
+    shared_text = data
+    emit('sync_text', shared_text, broadcast=True)  # إرسال النص لجميع المتصلين
+
+# استقبال أمر الزر لتشغيل/إيقاف المزامنة
 @socketio.on('toggle_sync')
-def toggle_sync():
-    global sync_enabled
-    sync_enabled = not sync_enabled
-    emit('sync_status', {'enabled': sync_enabled}, broadcast=True)
+def handle_toggle_sync(data):
+    emit('sync_status', data, broadcast=True)
 
-@socketio.on('action')
-def handle_action(data):
-    if sync_enabled:
-        emit('action', data, broadcast=True, include_self=False)
-
-if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    socketio.run(app, host="0.0.0.0", port=5000, allow_unsafe_werkzeug=True)
